16127acc7f0528716bca25b654a87049b5f7da20
AlexAplin <aplin.alex@gmail.com>
2019-11-27 22:00:43 -0500

    Bump to v1.4

--- nndownload/nndownload.py
+++ nndownload/nndownload.py
@@ -27,7 +27,7 @@ import urllib.parse
 import websockets
 import xml.dom.minidom
 
-__version__ = "1.3"
+__version__ = "1.4"
 __author__ = "Alex Aplin"
 __copyright__ = "Copyright 2019 Alex Aplin"
 __license__ = "MIT"
