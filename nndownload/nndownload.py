4628305ef15e912b44e8dfaf13b93af5718a28d3
AlexAplin <aplin.alex@gmail.com>
2021-03-22 02:01:34 -0400

    Bump to v1.9

--- nndownload/nndownload.py
+++ nndownload/nndownload.py
@@ -29,7 +29,7 @@ import traceback
 import urllib.parse
 import xml.dom.minidom
 
-__version__ = "1.8.2"
+__version__ = "1.9"
 __author__ = "Alex Aplin"
 __copyright__ = "Copyright 2021 Alex Aplin"
 __license__ = "MIT"
