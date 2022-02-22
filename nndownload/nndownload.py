9a89bd2801cb524e55e08de778e02f1e2a2a48eb
Alex Aplin <AlexAplin@users.noreply.github.com>
2022-02-22 03:21:37 -0500

    OTP code in auth flow (#118) (#119)

--- nndownload/nndownload.py
+++ nndownload/nndownload.py
@@ -37,7 +37,8 @@ MODULE_NAME = "nndownload"
 HOST = "nicovideo.jp"
 
 MY_URL = "https://www.nicovideo.jp/my"
-LOGIN_URL = "https://account.nicovideo.jp/api/v1/login?site=niconico"
+# LOGIN_URL = "https://account.nicovideo.jp/api/v1/login?site=niconico"
+LOGIN_URL = "https://account.nicovideo.jp/login/redirector?show_button_twitter=1&site=niconico&show_button_facebook=1&sec=header_pc&next_url=/"
 VIDEO_URL = "https://nicovideo.jp/watch/{0}"
 NAMA_URL = "https://live.nicovideo.jp/watch/{0}"
 CHANNEL_VIDEOS_URL = "https://ch.nicovideo.jp/{0}/video?page={1}"
@@ -1642,11 +1643,36 @@ def login(username: str, password: str, session_cookie: str) -> requests.Session
 
             login_request = session.post(LOGIN_URL, data=login_post)
             login_request.raise_for_status()
+
+            if ("message=cant_login" in login_request.url):
+                raise AuthenticationException("Incorrect email/telephone or password. Please verify your login details")
+
+            otp_code_request = session.get(login_request.url)
+            otp_code_page = BeautifulSoup(otp_code_request.text, "html.parser")
+            otp_code_account = otp_code_page.select("div.pageMainMsg span.userAccount")[0].text
+
+            otp_requests_made = 0
+            while otp_requests_made < 10 and not session.cookies.get_dict().get("user_session", None):
+                otp_code = input("Enter the OTP code sent to the email/telephone on file for your account ({}): ".format(otp_code_account))
+                otp_code = otp_code.strip()
+
+                otp_post = {
+                    "otp": otp_code,
+                    "device_name": f"{MODULE_NAME}/{__version__}"
+                }
+
+                otp_post_request = session.post(login_request.url, data=otp_post)
+                otp_requests_made += 1
+                otp_post_request.raise_for_status()
+
+                if not session.cookies.get_dict().get("user_session", None):
+                    output("Failed to login. Please verify your OTP code and try again.\n", logging.INFO)
+
             if not session.cookies.get_dict().get("user_session", None):
-                output("Failed to login.\n", logging.INFO)
-                raise AuthenticationException("Failed to login. Please verify your account email/telephone and password")
+                raise AuthenticationException("Failed to login. Please verify your email/telephone, password, and OTP code")
 
             output("Logged in.\n", logging.INFO)
+
         else:
             output("Using provided session cookie.\n", logging.INFO)
 
